2e988b425712c5bf32a785f74b3182bb4f0c9fec
苍雪 <694426204@qq.com>
2026-07-22 23:08:31 +0800

    这是中文修改版

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -4,25 +4,39 @@ let BeautifulJekyllJS = {
 
   bigImgEl : null,
   numImgs : null,
+  $navbar : null,
+  scrollTimeout : null,
+  imgCycleTimer : null,
+  imgCycleRunning : false,
+  escapeHandler : null,
 
   init : function() {
+    // Cache DOM elements
+    BeautifulJekyllJS.$navbar = $(".navbar");
+
     setTimeout(BeautifulJekyllJS.initNavbar, 10);
 
     // Shorten the navbar after scrolling a little bit down
+    // Throttled scroll handler
     $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar").addClass("top-nav-short");
-        } else {
-            $(".navbar").removeClass("top-nav-short");
-        }
+      if (BeautifulJekyllJS.scrollTimeout === null) {
+        BeautifulJekyllJS.scrollTimeout = setTimeout(function() {
+          BeautifulJekyllJS.scrollTimeout = null;
+          if (BeautifulJekyllJS.$navbar.offset().top > 50) {
+            BeautifulJekyllJS.$navbar.addClass("top-nav-short");
+          } else {
+            BeautifulJekyllJS.$navbar.removeClass("top-nav-short");
+          }
+        }, 16); // ~60fps
+      }
     });
 
     // On mobile, hide the avatar when expanding the navbar menu
     $('#main-navbar').on('show.bs.collapse', function () {
-      $(".navbar").addClass("top-nav-expanded");
+      BeautifulJekyllJS.$navbar.addClass("top-nav-expanded");
     });
     $('#main-navbar').on('hidden.bs.collapse', function () {
-      $(".navbar").removeClass("top-nav-expanded");
+      BeautifulJekyllJS.$navbar.removeClass("top-nav-expanded");
     });
 
     // On mobile, collapse the navbar after selecting a menu item
@@ -34,20 +48,36 @@ let BeautifulJekyllJS = {
     BeautifulJekyllJS.initImgs();
 
     BeautifulJekyllJS.initSearch();
+
+    // Pause image cycle when page is hidden
+    document.addEventListener('visibilitychange', function() {
+      if (document.hidden) {
+        BeautifulJekyllJS.pauseImgCycle();
+      } else {
+        BeautifulJekyllJS.resumeImgCycle();
+      }
+    });
   },
 
   initNavbar : function() {
     // Set the navbar-dark/light class based on its background color
-    const rgb = $('.navbar').css("background-color").replace(/[^\d,]/g,'').split(",");
+    const bgColor = BeautifulJekyllJS.$navbar.css("background-color");
+    if (!bgColor || bgColor === "transparent" || bgColor === "rgba(0, 0, 0, 0)") {
+      return;
+    }
+    const rgb = bgColor.replace(/[^\d,]/g,'').split(",");
+    if (!rgb || rgb.length < 3 || isNaN(parseInt(rgb[0])) || isNaN(parseInt(rgb[1])) || isNaN(parseInt(rgb[2]))) {
+      return;
+    }
     const brightness = Math.round(( // http://www.w3.org/TR/AERT#color-contrast
       parseInt(rgb[0]) * 299 +
       parseInt(rgb[1]) * 587 +
       parseInt(rgb[2]) * 114
     ) / 1000);
     if (brightness <= 125) {
-      $(".navbar").removeClass("navbar-light").addClass("navbar-dark");
+      BeautifulJekyllJS.$navbar.removeClass("navbar-light").addClass("navbar-dark");
     } else {
-      $(".navbar").removeClass("navbar-dark").addClass("navbar-light");
+      BeautifulJekyllJS.$navbar.removeClass("navbar-dark").addClass("navbar-light");
     }
   },
 
@@ -64,37 +94,65 @@ let BeautifulJekyllJS = {
       const desc = imgInfo.desc;
       BeautifulJekyllJS.setImg(src, desc);
 
-      // For better UX, prefetch the next image so that it will already be loaded when we want to show it
-      const getNextImg = function() {
-        const imgInfo = BeautifulJekyllJS.getImgInfo();
-        const src = imgInfo.src;
-        const desc = imgInfo.desc;
-
-        const prefetchImg = new Image();
-        prefetchImg.src = src;
-        // if I want to do something once the image is ready: `prefetchImg.onload = function(){}`
-
-        setTimeout(function(){
-          const img = $("<div></div>").addClass("big-img-transition").css("background-image", 'url(' + src + ')');
-          $(".intro-header.big-img").prepend(img);
-          setTimeout(function(){ img.css("opacity", "1"); }, 50);
-
-          // after the animation of fading in the new image is done, prefetch the next one
-          //img.one("transitioned webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
-          setTimeout(function() {
-            BeautifulJekyllJS.setImg(src, desc);
-            img.remove();
-            getNextImg();
-          }, 1000);
-          //});
-        }, 6000);
-      };
-
       // If there are multiple images, cycle through them
       if (BeautifulJekyllJS.numImgs > 1) {
-        getNextImg();
+        BeautifulJekyllJS.startImgCycle();
       }
     }
+  },
+
+  startImgCycle : function() {
+    if (BeautifulJekyllJS.imgCycleRunning) {
+      return;
+    }
+    BeautifulJekyllJS.imgCycleRunning = true;
+    BeautifulJekyllJS.scheduleNextImg();
+  },
+
+  pauseImgCycle : function() {
+    if (BeautifulJekyllJS.imgCycleTimer) {
+      clearTimeout(BeautifulJekyllJS.imgCycleTimer);
+      BeautifulJekyllJS.imgCycleTimer = null;
+    }
+    BeautifulJekyllJS.imgCycleRunning = false;
+  },
+
+  resumeImgCycle : function() {
+    if (BeautifulJekyllJS.numImgs > 1 && !BeautifulJekyllJS.imgCycleRunning) {
+      BeautifulJekyllJS.startImgCycle();
+    }
+  },
+
+  scheduleNextImg : function() {
+    if (!BeautifulJekyllJS.imgCycleRunning) {
+      return;
+    }
+    // For better UX, prefetch the next image so that it will already be loaded when we want to show it
+    const imgInfo = BeautifulJekyllJS.getImgInfo();
+    const src = imgInfo.src;
+    const desc = imgInfo.desc;
+
+    const prefetchImg = new Image();
+    prefetchImg.src = src;
+
+    BeautifulJekyllJS.imgCycleTimer = setTimeout(function(){
+      if (!BeautifulJekyllJS.imgCycleRunning) {
+        return;
+      }
+      const img = $("<div></div>").addClass("big-img-transition").css("background-image", 'url(' + src + ')');
+      $(".intro-header.big-img").prepend(img);
+      setTimeout(function(){ img.css("opacity", "1"); }, 50);
+
+      // after the animation of fading in the new image is done, prefetch the next one
+      BeautifulJekyllJS.imgCycleTimer = setTimeout(function() {
+        if (!BeautifulJekyllJS.imgCycleRunning) {
+          return;
+        }
+        BeautifulJekyllJS.setImg(src, desc);
+        img.remove();
+        BeautifulJekyllJS.scheduleNextImg();
+      }, 1000);
+    }, 6000);
   },
 
   getImgInfo : function() {
@@ -127,18 +185,30 @@ let BeautifulJekyllJS = {
       $("#beautifuljekyll-search-overlay").show();
       $("#nav-search-input").focus().select();
       $("body").addClass("overflow-hidden");
-    });
-    $("#nav-search-exit").click(function(e) {
-      e.preventDefault();
-      $("#beautifuljekyll-search-overlay").hide();
-      $("body").removeClass("overflow-hidden");
-    });
-    $(document).on('keyup', function(e) {
-      if (e.key == "Escape") {
-        $("#beautifuljekyll-search-overlay").hide();
-        $("body").removeClass("overflow-hidden");
+      // Attach escape key handler
+      if (!BeautifulJekyllJS.escapeHandler) {
+        BeautifulJekyllJS.escapeHandler = function(e) {
+          if (e.key == "Escape") {
+            BeautifulJekyllJS.closeSearch();
+          }
+        };
+        $(document).on('keyup', BeautifulJekyllJS.escapeHandler);
       }
     });
+
+    $("#nav-search-exit").click(function(e) {
+      e.preventDefault();
+      BeautifulJekyllJS.closeSearch();
+    });
+  },
+
+  closeSearch : function() {
+    $("#beautifuljekyll-search-overlay").hide();
+    $("body").removeClass("overflow-hidden");
+    if (BeautifulJekyllJS.escapeHandler) {
+      $(document).off('keyup', BeautifulJekyllJS.escapeHandler);
+      BeautifulJekyllJS.escapeHandler = null;
+    }
   }
 };
 
